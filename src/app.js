b11fa921220a165adb86ef058239795bf3e30662
Raquel Moss <raquelxmoss@gmail.com>
2016-01-11 20:36:43 +1300

    Render board

--- src/app.js
+++ src/app.js
@@ -1,8 +1,36 @@
 import {Observable} from 'rx';
 import {div} from '@cycle/dom';
+import _ from 'lodash';
+
+const board = _.range(0, 15).map(function(){
+  return _.range(0, 15).map(() => "")
+})
+
+board[5][4] = "A"
+
+function renderTile(tile) {
+  return (
+    div('.tile', tile)
+  )
+}
+
+function renderRow(row) {
+  return (
+    div('.row', row.map(renderTile))
+  )
+}
+
+
+function renderBoard (board) {
+  return (
+    div('.board', board.map(renderRow))
+  )
+}
 
 export default function App ({DOM}) {
+  const state$ = Observable.just(board)
+
   return {
-    DOM: Observable.just(div('.hello-world', 'Change me!'))
+    DOM: state$.map(renderBoard)
   };
 }
