8126e8e40f89621fcf01a9ad4fab4e054ba8202c
Raquel Moss <raquelxmoss@gmail.com>
2016-01-11 21:17:22 +1300

    Move tiles from hand to board

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import {Observable} from 'rx';
 import {div} from '@cycle/dom';
 import _ from 'lodash';
+import $ from 'jquery';
 
 const board = _.range(0, 15).map(function(){
   return _.range(0, 15).map(() => "")
@@ -10,7 +11,8 @@ board[5][4] = "A"
 
 const initialState = {
   board,
-  hand: ["A", "E", "I", "O", "U"]
+  hand: ["A", "E", "I", "O", "U"],
+  selectedTile: null
 }
 
 function renderTile(tile) {
@@ -38,8 +40,52 @@ function renderHand (hand) {
   )
 }
 
+function makeSelectHandTileReducer (event) {
+  return function selectHandTile (state) {
+    const selectedTile = $(event.target).index()
+
+    return Object.assign({}, state, {selectedTile})
+  }
+}
+
+function makePlaceTileReducer (event) {
+  return function placeTile (state) {
+    const row = $(event.target).parent().index()
+    const column = $(event.target).index()
+
+    state.board[row][column] = state.hand[state.selectedTile]
+    state.hand.splice(state.selectedTile, 1)
+    state.selectedTile = null
+
+    return state
+  }
+}
+
 export default function App ({DOM}) {
-  const state$ = Observable.just(initialState)
+
+  const selectHandTile$ = DOM
+    .select('.hand .tile')
+    .events('click')
+
+  const boardClick$ = DOM
+    .select('.board .tile')
+    .events('click')
+
+  const placeTileReducer$ = boardClick$
+    .map(e => makePlaceTileReducer(e))
+
+  const selectHandTileReducer$ = selectHandTile$
+    .map(e => makeSelectHandTileReducer(e))
+
+  const reducer$ = Observable.merge(
+    selectHandTileReducer$,
+    placeTileReducer$
+  )
+
+  const state$ = reducer$
+    .startWith(initialState)
+    .scan((state, reducer) => reducer(state))
+    .do(console.log.bind(console, 'state'))
 
   return {
     DOM: state$.map(({board, hand}) => (
