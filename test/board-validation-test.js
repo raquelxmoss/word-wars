2f3f84fafdfe89934eee3454f42c92e5190d0a5f
Nick Johnstone <ncwjohnstone@gmail.com>
2016-02-08 13:48:09 +1300

    Add more tests for column validation

--- test/board-validation-test.js
+++ test/board-validation-test.js
@@ -8,6 +8,14 @@ function makeRow (rowString) {
     .map(letter => ({letter: letter === ' ' ? '' : letter, active: false}))
 }
 
+function makeBoard(board) {
+  const rows = board.split('\n');
+
+  const whitespaceAmount = 6;
+
+  return rows.map(row => row.slice(whitespaceAmount)).map(makeRow).filter(row => row.length >= 1);
+}
+
 describe('validateBoard', () => {
   it('sets the validity of each letter of the board', () => {
     const board = [
@@ -65,19 +73,65 @@ describe('validateBoard', () => {
       t
     `);
 
-    function makeBoard(board) {
-      const rows = board.split('\n');
+    const validatedBoard = validateBoard(board);
 
-      const whitespaceAmount = 6;
+    assert.equal(validatedBoard[0][0].active, false, 'first empty tile is invalid');
+    assert.equal(validatedBoard[1][0].active, true, 'rat in column is valid');
+    assert.equal(validatedBoard[3][0].active, true, 'rat in column is valid');
+  });
 
-      return rows.map(row => row.slice(whitespaceAmount)).map(makeRow).filter(row => row.length >= 1);
-    }
+  it('handles columns', () => {
+    const board = makeBoard(`
+       
+      r
+      a
+      t
+       
+      s
+      n
+      o
+      w
+       
+       
+    `);
 
     const validatedBoard = validateBoard(board);
 
     assert.equal(validatedBoard[0][0].active, false, 'first empty tile is invalid');
     assert.equal(validatedBoard[1][0].active, true, 'rat in column is valid');
     assert.equal(validatedBoard[3][0].active, true, 'rat in column is valid');
+
+    assert.equal(validatedBoard[4][0].active, false, 'second empty tile is invalid');
+    assert.equal(validatedBoard[5][0].active, true, 'snow in column is valid');
+    assert.equal(validatedBoard[8][0].active, true, 'snow in column is valid');
+    assert.equal(validatedBoard[9][0].active, false, 'last empty tile is invalid');
+  });
+
+  it('handles invalid words in columns', () => {
+    const board = makeBoard(`
+       
+      r
+      a
+      t
+       
+      s
+      n
+      i
+      w
+       
+       
+    `);
+
+    const validatedBoard = validateBoard(board);
+
+    assert.equal(validatedBoard[0][0].active, false, 'first empty tile is invalid');
+    assert.equal(validatedBoard[1][0].active, true, 'rat in column is valid');
+    assert.equal(validatedBoard[3][0].active, true, 'rat in column is valid');
+
+    assert.equal(validatedBoard[4][0].active, false, 'second empty tile is invalid');
+    assert.equal(validatedBoard[5][0].active, false, 'sniw in column is invalid');
+    assert.equal(validatedBoard[8][0].active, false, 'sniw in column is invalid');
+    assert.equal(validatedBoard[9][0].active, false, 'last empty tile is invalid');
   });
 });
 
