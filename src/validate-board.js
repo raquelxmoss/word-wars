ff13c302bc4ae2fd7232909cad9ef31e8f4f27ee
Nick Johnstone <ncwjohnstone@gmail.com>
2016-02-08 13:31:20 +1300

    Semistandard

--- src/validate-board.js
+++ src/validate-board.js
@@ -4,52 +4,52 @@ function validateRow (row) {
   const wordsInRow = extractWords(row);
 
   return row.map((square, index) => {
-    if(square.letter !== '') {
+    if (square.letter !== '') {
       const wordToValidate = findWordToValidate(wordsInRow, index);
 
       return Object.assign({}, square, {active: validateWord(wordToValidate)});
     }
 
-    return square
+    return square;
   });
 }
 
 function findWordToValidate (wordsInRow, index) {
-  return wordsInRow.filter(word => word.startIndex <= index && word.endIndex >= index)[0]
+  return wordsInRow.filter(word => word.startIndex <= index && word.endIndex >= index)[0];
 }
 
 function extractWords (row) {
   const wordsInRow = row.reduce((extractedWords, square, index) => {
-    if(square.letter === '' && !extractedWords.currentlyInWord) { return extractedWords };
+    if (square.letter === '' && !extractedWords.currentlyInWord) { return extractedWords; }
 
-    if(square.letter === '' && extractedWords.currentlyInWord) {
-      extractedWords.currentlyInWord = false
-      extractedWords[extractedWords.length - 1].endIndex = index - 1
+    if (square.letter === '' && extractedWords.currentlyInWord) {
+      extractedWords.currentlyInWord = false;
+      extractedWords[extractedWords.length - 1].endIndex = index - 1;
 
-      return extractedWords
-    };
+      return extractedWords;
+    }
 
-    if(extractedWords.currentlyInWord) {
-      extractedWords[extractedWords.length - 1].push(square.letter)
+    if (extractedWords.currentlyInWord) {
+      extractedWords[extractedWords.length - 1].push(square.letter);
     } else {
-      const newWord = [square.letter]
-      newWord.startIndex = index
+      const newWord = [square.letter];
+      newWord.startIndex = index;
 
-      extractedWords.push(newWord)
-      extractedWords.currentlyInWord = true
-    };
+      extractedWords.push(newWord);
+      extractedWords.currentlyInWord = true;
+    }
 
-    return extractedWords
-  }, [])
+    return extractedWords;
+  }, []);
 
-  if(wordsInRow.currentlyInWord) {
-    wordsInRow.currentlyInWord = false
-    wordsInRow[wordsInRow.length - 1].endIndex = row.length - 1
-  };
+  if (wordsInRow.currentlyInWord) {
+    wordsInRow.currentlyInWord = false;
+    wordsInRow[wordsInRow.length - 1].endIndex = row.length - 1;
+  }
 
-  return wordsInRow
+  return wordsInRow;
 }
 
-export default function validateBoard(board) {
-  return board.map(validateRow)
+export default function validateBoard (board) {
+  return board.map(validateRow);
 }
